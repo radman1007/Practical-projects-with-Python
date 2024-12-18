44c60a308c83ae663364957410d3b701373e2718
radman1007 <radman.cod110@gmail.com>
2024-12-18 19:34:45 +0330

    set color and add screen

--- Coin Collector/main.py
+++ Coin Collector/main.py
@@ -1,3 +1,15 @@
 import pygame
 import random
 
+pygame.init()
+
+screen_width = 800
+screen_height = 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("بازی سکه جمع کن")
+
+black = (0, 0, 0)
+gray = (128, 128, 128)
+white = (255, 255, 255)
+red = (255, 0, 0)
+yellow = (255, 255, 0)
