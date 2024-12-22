2c01f863fa9e56fcc1cee934e3112df8a5a160a4
radman1007 <radman.cod110@gmail.com>
2024-12-23 00:28:33 +0330

    open and write templates with jinja2

--- Web/PDF Invoice/main.py
+++ Web/PDF Invoice/main.py
@@ -2,3 +2,8 @@ from jinja2 import Environment, FileSystemLoader
 import jdatetime
 
 env = Environment(loader=FileSystemLoader("templates"))
+template = env.get_template("index.html")
+output = template.render(name="index")
+with open(r"templates/new-template.html", mode="w", encoding='utf-8') as tm:
+    tm.write(output)
+    
